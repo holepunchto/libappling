b6afad9fbffa5ff79d51427118aaf6fa56e86d68
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-11-30 13:35:56 +0100

    `Example.AppDir` -> `Example` on Windows

--- test/fixtures/app.h
+++ test/fixtures/app.h
@@ -4,7 +4,7 @@
 #if defined(APPLING_OS_LINUX)
 #define APPLING_TEST_EXE "Example.AppDir/Example"
 #elif defined(APPLING_OS_WIN32)
-#define APPLING_TEST_EXE "Example.AppDir\\Example.exe"
+#define APPLING_TEST_EXE "Example\\Example.exe"
 #elif defined(APPLING_OS_DARWIN)
 #define APPLING_TEST_EXE "Example.app/Contents/MacOS/Example"
 #endif

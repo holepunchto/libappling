817e5f935852c9128a0a609a743c6e4f22b8f104
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-12-07 11:50:07 +0100

    Fix Linux app test fixture path

--- test/fixtures/app.h
+++ test/fixtures/app.h
@@ -1,12 +1,12 @@
 #ifndef APPLING_TEST_FIXTURES_APP_H
 #define APPLING_TEST_FIXTURES_APP_H
 
-#if defined(APPLING_OS_LINUX)
-#define APPLING_TEST_EXE "Example.AppDir/Example"
+#if defined(APPLING_OS_DARWIN)
+#define APPLING_TEST_EXE "Example.app/Contents/MacOS/Example"
+#elif defined(APPLING_OS_LINUX)
+#define APPLING_TEST_EXE "Example.AppDir/usr/bin/example"
 #elif defined(APPLING_OS_WIN32)
 #define APPLING_TEST_EXE "Example\\Example.exe"
-#elif defined(APPLING_OS_DARWIN)
-#define APPLING_TEST_EXE "Example.app/Contents/MacOS/Example"
 #endif
 
 #endif // APPLING_TEST_FIXTURES_APP_H

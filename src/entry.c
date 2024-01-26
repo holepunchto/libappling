84594ebc4b85040ba48211faadd0be15532ec944
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-01-26 09:57:57 +0100

    Use `execv()` in default launcher

--- src/entry.c
+++ src/entry.c
@@ -1,19 +1,15 @@
-#include <assert.h>
 #include <log.h>
 #include <path.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
-#include <utf.h>
-#include <uv.h>
+
+#if defined(APPLING_OS_WIN32)
+#include <process.h>
+#else
+#include <unistd.h>
+#endif
 
 #include "../include/appling.h"
-
-static void
-on_process_exit (uv_process_t *handle, int64_t exit_status, int term_signal) {
-  *((int64_t *) handle->data) = exit_status;
-
-  uv_close((uv_handle_t *) handle, NULL);
-}
 
 int
 appling_launch_v0 (const appling_launch_info_t *info) {
@@ -47,12 +43,9 @@ appling_launch_v0 (const appling_launch_info_t *info) {
   appling_path_t appling;
 
 #if defined(APPLING_OS_LINUX)
-  appling_path_t appimage;
-  path_len = sizeof(appling_path_t);
+  char *appimage = getenv("APPIMAGE");
 
-  err = uv_os_getenv("APPIMAGE", appimage, &path_len);
-
-  strcpy(appling, err == 0 ? appimage : app->path);
+  strcpy(appling, appimage ? appimage : app->path);
 #else
   strcpy(appling, app->path);
 #endif
@@ -75,42 +68,11 @@ appling_launch_v0 (const appling_launch_info_t *info) {
 
   char *args[] = {file, "-", "--appling", appling, "--launch", launch, NULL};
 
-  uv_loop_t loop;
-  err = uv_loop_init(&loop);
-  if (err < 0) goto err;
-
-  int64_t exit_status = 0;
-
-  uv_process_t process;
-
-  process.data = (void *) &exit_status;
-
-  uv_process_options_t options = {
-    .exit_cb = on_process_exit,
-    .file = file,
-    .args = args,
-    .flags = UV_PROCESS_WINDOWS_HIDE,
-    .stdio_count = 3,
-    .stdio = (uv_stdio_container_t[]){
-      {.flags = UV_INHERIT_FD, .data.fd = 0},
-      {.flags = UV_INHERIT_FD, .data.fd = 1},
-      {.flags = UV_INHERIT_FD, .data.fd = 2},
-    },
-  };
-
-  err = uv_spawn(&loop, &process, &options);
-  if (err < 0) goto err;
-
-  uv_run(&loop, UV_RUN_DEFAULT);
-
-  err = uv_loop_close(&loop);
-  assert(err == 0);
-
-  return exit_status;
-
-err:
-  err = uv_loop_close(&loop);
-  assert(err == 0);
+#if defined(APPLING_OS_WIN32)
+  err = _execv(file, args);
+#else
+  err = execv(file, args);
+#endif
 
   return err;
 }

5bc5b4cf85274474e0c71605292238b8bca824f4
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-02-09 17:17:47 +0100

    `appling_open()` should block until launched

--- src/darwin/open.m
+++ src/darwin/open.m
@@ -24,14 +24,30 @@ appling_open (const appling_app_t *app, const char *argument) {
 
     NSWorkspaceOpenConfiguration *configuration = [NSWorkspaceOpenConfiguration configuration];
 
+    configuration.createsNewApplicationInstance = YES;
+
     if (argument) {
       configuration.arguments = @[ [NSString stringWithFormat:@"%s", argument] ];
     }
 
+    __block NSError *error = Nil;
+
+    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
+
     [[NSWorkspace sharedWorkspace]
       openApplicationAtURL:path
              configuration:configuration
-         completionHandler:nil];
+         completionHandler:^(NSRunningApplication *app, NSError *err) {
+           error = err;
+
+           dispatch_semaphore_signal(sem);
+         }];
+
+    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
+
+    dispatch_release(sem);
+
+    if (error) return -1;
   }
 
   return 0;

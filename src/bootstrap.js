e770bb14681e0cea28511bee48ef1972cb4e1735
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-09-30 10:55:26 +0200

    Remove error handlers

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,16 +1,16 @@
 /* global Bare, Appling */
 
-const onerror = (err) => {
-  console.error(err.stack)
+// const onerror = (err) => {
+//   console.error(err.stack)
 
-  Appling.error(err.stack)
+//   Appling.error(err.stack)
 
-  Bare.exit(1) // Cleanly exit and return to C
-}
+//   Bare.exit(1) // Cleanly exit and return to C
+// }
 
 Bare
-  .on('uncaughtException', onerror)
-  .on('unhandledRejection', onerror)
+  // .on('uncaughtException', onerror)
+  // .on('unhandledRejection', onerror)
   .on('exit', () => {
     console.error('Bare is exiting')
   })

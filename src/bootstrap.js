6e66b418ebe6ab929c112c1594d76df4f580db85
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-09-30 09:50:53 +0200

    Log on exit and reduce test timeout

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -9,5 +9,8 @@ const onerror = (err) => {
 Bare
   .on('uncaughtException', onerror)
   .on('unhandledRejection', onerror)
+  .on('exit', () => {
+    console.error('Bare is exiting')
+  })
 
 require('pear-updater-bootstrap')(Buffer.from(Appling.dkey), Appling.directory, { lock: false })

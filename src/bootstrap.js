bdf0afe457f3c7d488cd23add2da6fa28ca6970c
Andrei Regiani <andrei.cpp@gmail.com>
2025-07-21 17:58:41 +0700

    add pear-distributable-bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -9,3 +9,9 @@ Bare.on('uncaughtException', onerror).on('unhandledRejection', onerror)
 require('pear-updater-bootstrap')(Buffer.from(Appling.key), Appling.directory, {
   lock: false
 })
+
+require('pear-distributable-bootstrap')({
+  pearKey: Buffer.from(Appling.key),
+  pearDir: Appling.directory,
+  appLink: 'pear://yx8yxsegdyow6kawrruhsx5k7scampx5kww1d7py5ebjf1gm5sjo' // TEMP: keet-next hardcoded
+})

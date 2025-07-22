04bbc346cbe2259480ae5c4327cfe9220e6f8e7b
Andrei Regiani <andrei.cpp@gmail.com>
2025-07-23 01:08:00 +0700

    formatting

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -10,11 +10,10 @@ async function bootstrap () {
   await require('pear-updater-bootstrap')(Buffer.from(Appling.key), Appling.directory, {
     lock: false
   })
-
   await require('pear-distributable-bootstrap')({
     pearKey: Buffer.from(Appling.key),
     pearDir: Appling.directory,
-    appLink: 'pear://yx8yxsegdyow6kawrruhsx5k7scampx5kww1d7py5ebjf1gm5sjo'
+    appLink: 'pear://yx8yxsegdyow6kawrruhsx5k7scampx5kww1d7py5ebjf1gm5sjo' // TEMP: hardcoded keet-next
   })
 }
 bootstrap()

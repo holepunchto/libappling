2accaa44a736119ffa1dd651c9a58e347d73a7d8
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-11-27 09:10:59 +0100

    Check for `--json` in test as well

--- test/fixtures/runtime.c
+++ test/fixtures/runtime.c
@@ -11,7 +11,8 @@ main(int argc, char *argv[]) {
     argc >= 5 &&
     strcmp(argv[1], "run") == 0 &&
     strcmp(argv[2], "--trusted") == 0 &&
-    strcmp(argv[3], "--preflight") == 0
+    strcmp(argv[3], "--preflight") == 0 &&
+    strcmp(argv[4], "--json") == 0
   ) {
     for (int i = 0, n = 10; i < n; i++) {
       printf(
